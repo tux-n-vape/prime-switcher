146e9c8f4291b1f9f545d8d35bb7acea6ba654c2
Adrien Jussak <adrien.jussak@wascardev.com>
2019-02-21 21:35:52 +0100

    Driver switch support

--- prime-swticher.py
+++ prime-swticher.py
@@ -4,24 +4,28 @@ import argparse
 import switchers
 import gui
 import os
-
-default_driver = 'free'
+import utils
 
 
 def run_as_root(func, *args, **kwargs):
     if os.getuid() == 0:
         func(*args, **kwargs)
     else:
-        print('Root is required')
+        raise PermissionError('Root permissions are required.')
 
 
 if __name__ == '__main__':
+    current_driver_file = utils.get_config_filepath('current-driver')
+
+    with open(current_driver_file) as f:
+        default_driver = f.read()
+
     switchers_dict = {'free': switchers.OpenSourceDriverSwitcher(), 'nvidia': switchers.NvidiaSwitcher()}
 
     parser = argparse.ArgumentParser(prog='prime-switcher')
     parser.add_argument('--set', '-s', type=str, choices=['power-saving', 'performance'])
     parser.add_argument('--driver', '-d', type=str, choices=switchers_dict.keys(), default=default_driver)
-    parser.add_argument('--uninstall', action='store_true')
+    parser.add_argument('--uninstall', '-u', action='store_true')
     parser.add_argument('--gui', action='store_true')
 
     args = parser.parse_args()
@@ -33,6 +37,8 @@ if __name__ == '__main__':
         if args.uninstall:
             run_as_root(swr.uninstall)
         elif args.set is not None:
+            if args.driver != default_driver:
+                run_as_root(switchers_dict[default_driver].uninstall)
             run_as_root(swr.set_dedicated_gpu_state, args.set == 'performance')
         else:
             parser.print_help()
