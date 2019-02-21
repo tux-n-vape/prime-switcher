ff59a797d9c4e520ee68db3550ea0555cf899c54
Adrien Jussak <adrien.jussak@wascardev.com>
2019-02-22 00:00:11 +0100

    Command Linux Text

--- prime-swticher.py
+++ prime-swticher.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     current_driver_file = utils.get_config_filepath('current-driver')
 
     with open(current_driver_file, 'r') as f:
-        default_driver = f.read()
+        default_driver = f.readline()
 
     switchers_dict = {'free': switchers.OpenSourceDriverSwitcher(), 'nvidia': switchers.NvidiaSwitcher()}
 
@@ -27,21 +27,29 @@ if __name__ == '__main__':
     parser.add_argument('--driver', '-d', type=str, choices=switchers_dict.keys(), default=default_driver)
     parser.add_argument('--uninstall', '-u', action='store_true')
     parser.add_argument('--gui', action='store_true')
+    parser.add_argument('--query', '-q', action='store_true')
 
     args = parser.parse_args()
+    swr = switchers_dict[args.driver]
     if args.gui:
         gui.open_gui()
+    elif args.query:
+        gpu = 'Performances' if swr.get_dedicated_gpu_state() else 'Power Saving'
+        print('Current Mode : ', gpu)
+        print('GPU : ', swr.get_current_gpu_name())
     else:
-        swr = switchers_dict[args.driver]
-
         if args.uninstall:
+            print('Uninstalling...')
             run_as_root(swr.uninstall)
+            print('Done.')
         elif args.set is not None:
             if args.driver != default_driver:
+                print('Uninstalling config for previous driver...')
                 run_as_root(switchers_dict[default_driver].uninstall)
                 with open(current_driver_file, 'w') as f:
                     f.write(args.driver)
-
+            print('Configuring...')
             run_as_root(swr.set_dedicated_gpu_state, args.set == 'performance')
+            print('Done.')
         else:
             parser.print_help()
