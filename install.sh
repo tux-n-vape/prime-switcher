f66e26f9ce1edc02d58f85871d034a05e6490382
Adrien Jussak <jussak.adrien@gmail.com>
2019-02-27 00:33:40 +0100

    Installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /etc/prime-switcher/
-cp configs/* /etc/prime-switcher/
+cp -r configs/* /etc/prime-switcher/
 mkdir -p /usr/lib/prime-switcher/
 cp gui.py prime-switcher.py utils.py switchers.py /usr/lib/prime-switcher/
 ln -s /usr/lib/prime-switcher/prime-switcher.py /usr/bin/prime-switcher
