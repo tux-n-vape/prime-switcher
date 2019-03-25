f35e9633fc689db1d045be2c79d309abda8b9016
Adrien Jussak <jussak.adrien@gmail.com>
2019-03-25 13:49:06 +0100

    Install script

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ mkdir -p /usr/share/prime-switcher/
 cp icons/* /usr/share/prime-switcher/
 cp gpu.py gui.py __main__.py utils.py switchers.py /usr/lib/prime-switcher/
 ln -s /usr/lib/prime-switcher/__main__.py /usr/bin/prime-switcher
+cp app-icon/* /usr/share/icons/hicolor/
+cp prime-switcher.desktop /etc/xdg/autostart/
 
 cd locales
 find . -name '*.mo' -exec cp --parents \{\} ${pkgdir}/usr/share/locales/ \;
